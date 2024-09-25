137167ecc4e9de6588564c905ae4924845e67b8a
Evan <evan.sherfey@stu.barren.kyschools.us>
2024-09-25 08:06:25 -0500

    Initial Commit

---
+++ Project_2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project_2
+//
+//  Created by Student on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
